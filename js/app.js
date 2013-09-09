deaa9d3fc2467d4c902ea2afcf0cccbde26cd0a6
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2013-09-09 16:04:35 +0200

    If there's an init method call it

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ var app = {
 };
 
 $(document).on("deviceready", function () {
+    if (typeof app.adapters.employee.initialize === "function") {
+        app.adapters.employee.initialize();
+    }
     app.router = new app.routers.AppRouter();
     app.utils.templates.load(["HomeView", "EmployeeView", "EmployeeListItemView", "ReportsView", "MapView"],
         function () {
